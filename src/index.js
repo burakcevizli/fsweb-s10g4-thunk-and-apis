552338daf59aaad56123db132de75f62ffeb998d
KrieqqeR <burakcevizli@gmail.com>
2023-09-01 02:18:27 +0300

    All is done .

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { Provider } from "react-redux";
-import { myReducer } from './reducers';
+import { myReducer } from "./reducers";
+import thunk from "redux-thunk";
 
-const depo = createStore(myReducer);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(myReducer, applyMiddleware(thunk));
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunk)
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={depo}>
+  <Provider store={store}>
     <BrowserRouter>
       <>
         <App />
@@ -21,7 +25,4 @@ root.render(
   </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
